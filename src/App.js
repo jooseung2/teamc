b00dd02ce5fdd3ef696abb097a50ce928d2a2dde
Shawn <seungchoi2019@u.northwestern.edu>
2020-01-30 21:26:11 -0600

    implemented projectcard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "rbx/index.css";
 import { Container } from "rbx";
 
-import ProjectCard from './components/ProjectCard/ProjectCard';
+import ProjectCardList from './components/ProjectCard/ProjectCardList';
 
-const App = () => (
-  <Container>
-    <ProjectCard />
-  </Container>
-);
+const App = () => {
+  const [data, setData] = useState({});
+  const [dataLoaded, setLoaded] = useState(false);
+  const database = Object.values(data);
+
+  useEffect (() => {
+    const fetchProjects = async () => {
+      const response = await fetch ('./data/data.json');
+      const json = await response.json();
+      setData(json);
+      setLoaded(true);
+    };
+    fetchProjects();
+  }, [])
+
+  return dataLoaded ?
+    (
+    <Container>
+      <ProjectCardList 
+        database = {database}
+      />
+    </Container>)
+  : (<p>loading hehehe</p>);
+};
 
 export default App;
